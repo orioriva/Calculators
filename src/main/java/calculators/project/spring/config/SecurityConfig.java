05a1959886aaf669906b113e4e694d7feb77206e
orioriva <toshikawa.syoichi@gmail.com>
2023-02-15 15:54:59 +0900

    2/15

--- src/main/java/calculators/project/spring/config/SecurityConfig.java
+++ src/main/java/calculators/project/spring/config/SecurityConfig.java
@@ -11,40 +11,41 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig{
 	@Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.formLogin(login -> login
-                .loginProcessingUrl("/login")
-                .loginPage("/login")
-                .defaultSuccessUrl("/mypage/top",true)	// ログイン後
-                .failureUrl("/login?error")
-                .permitAll()	// ログイン画面の未ログイン時のアクセス許可
-        ).logout(logout -> logout
-        		.logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .permitAll()
-        ).authorizeHttpRequests(authz -> authz
-        		.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-        		.mvcMatchers("/").permitAll()
-        		.mvcMatchers("/calculator").permitAll()
-        		.mvcMatchers("/register").permitAll()
-        		.mvcMatchers("/rest/users").permitAll()
-        		.mvcMatchers("/bbs").permitAll()
-        		.mvcMatchers("/rest/posts").permitAll()
-        		.mvcMatchers("/bbs/post").permitAll()
-        		.antMatchers("/h2-console/**").permitAll()
-        		.mvcMatchers("/admin").hasRole("ADMIN")
-        		.anyRequest().authenticated()
-        ).headers(header -> header
-        		.frameOptions()
-        		.disable()
-        ).csrf(csrf -> csrf
-        		.ignoringAntMatchers("/h2-console/**")
-        );
-        return http.build();
-    }
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.formLogin(login -> login
+			.loginProcessingUrl("/login")
+			.loginPage("/login")
+			.defaultSuccessUrl("/mypage/top",true)	// ログイン後
+			.failureUrl("/login?error")
+			.permitAll()	// ログイン画面の未ログイン時のアクセス許可
+		).logout(logout -> logout
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/login?logout")
+			.permitAll()
+		).authorizeHttpRequests(authz -> authz
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+			.mvcMatchers("/img/**").permitAll()
+			.mvcMatchers("/").permitAll()
+			.mvcMatchers("/calculator").permitAll()
+			.mvcMatchers("/register").permitAll()
+			.mvcMatchers("/rest/users").permitAll()
+			.mvcMatchers("/bbs").permitAll()
+			.mvcMatchers("/rest/posts").permitAll()
+			.mvcMatchers("/bbs/post").permitAll()
+			.antMatchers("/h2-console/**").permitAll()
+			.mvcMatchers("/admin").hasRole("ADMIN")
+			.anyRequest().authenticated()
+		).headers(header -> header
+			.frameOptions()
+			.disable()
+		).csrf(csrf -> csrf
+			.ignoringAntMatchers("/h2-console/**")
+		);
+		return http.build();
+	}
 }
