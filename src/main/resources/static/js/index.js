d9ce213118580eb718727329b890f45af7266751
orioriva <toshikawa.syoichi@gmail.com>
2023-02-16 15:51:23 +0900

    2/16

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,31 +1,13 @@
-'use strict'
-
-$('.slider').slick({
-    autoplay: true,       //自動再生
-    autoplaySpeed: 2000,  //自動再生のスピード
-    speed: 800,           //スライドするスピード
-    dots: true,           //スライド下のドット
-    arrows: true,         //左右の矢印
-    infinite: true,       //永久にループさせる
-});
-
-
-$("#main-slider").slick({
-	//ここにオプション
-	asNavFor:"#text-slider", //テキストスライダーを追従させる
-	autoplay:false,            //自動再生
-	arrows: true, // 前・次のボタンを表示する
-	dots: true,
-	arrows: true,
-	appendDots: $('.dots'),
-	speed: 1000, // スライドさせるスピード（ミリ秒）
-    slidesToShow: 1, // 表示させるスライド数
-    centerMode: true, // slidesToShowが奇数のとき、現在のスライドを中央に表示する
-    variableWidth: true, // スライド幅の自動計算を無効化
-});
-
-$("#text-slider").slick({
-    //ここにオプション
-    draggable:false,         //ドラッグでのスライド禁止
-    arrows:false             //矢印非表示
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    // ページネーションが必要なら追加
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    // ナビボタンが必要なら追加
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
 });
